bbfbed43b826f9306702787801aca0cdf70d667c
Wuyudaxia <1879233041@qq.com>
2024-07-05 20:35:47 +0800

    added class8

---
+++ class8/matrix.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Matrix{
+    
+    private:
+        int** arr;
+        int length_row;
+        int length_column;
+    
+    public:
+
+        // Rubic : A default constructor that initalize the `arr` and let every value inside to be 0
+        Matrix(int row, int col);
+        
+        // Rubic : A parametered constructor that initlize the `arr` and set every value to be `value`
+        Matrix(int row, int col, int value);
+
+        ~Matrix();
+
+        void printMatrix();
+
+        int getValue(int row, int col);
+
+        void addMatrix(Matrix& B);
+}
